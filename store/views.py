351f1ffe7d1a530b2c3b5a6f3a6dddc2a3ff8024
Rober <boblawyer1@gmail.com>
2025-07-29 20:23:45 -0300

    Implement pagination for product listing and update item count display

--- store/views.py
+++ store/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, get_object_or_404
+from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 from store.models import Product
 from category.models import Category
 from carts.models import CartItem
@@ -11,11 +12,21 @@ def store(request, category_slug = None):
     if category_slug != None:
         categories = get_object_or_404(Category, slug = category_slug)
         products = Product.objects.filter(category = categories, is_available=True)
+        paginator = Paginator(products, 1)
+        page = request.GET.get('page')
+        paged_products = paginator.get_page(page)
+        product_count = products.count()
     else:
         products = Product.objects.all().filter(is_available = True)
+        paginator = Paginator(products, 3)
+        page = request.GET.get('page')
+        paged_products = paginator.get_page(page)
+        product_count = products.count()
     
     return render(request, 'store/store.html', context = {
-        'products' : products
+        'products' : paged_products,
+        'product_count' : product_count
+
     })
 
 def product_detail(request, category_slug, product_slug):
