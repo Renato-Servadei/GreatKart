5acc520b37a93cd896d8a373fcf3308e47c5f27d
Rober <boblawyer1@gmail.com>
2025-07-30 15:58:37 -0300

    Add search functionality with dynamic URL routing and update templates for search results

--- store/views.py
+++ store/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render, get_object_or_404
+from django.db.models import Q
 from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
+from django.shortcuts import render, get_object_or_404
 from store.models import Product
 from category.models import Category
 from carts.models import CartItem
@@ -40,3 +41,15 @@ def product_detail(request, category_slug, product_slug):
         'in_cart' : in_cart
     }
     return render(request, 'store/product_detail.html', context)
+
+def search(request):
+    if 'keyword' in request.GET:
+        keyword = request.GET['keyword']
+        if keyword:
+            products = Product.objects.order_by('-created_date').filter(Q(description__icontains=keyword) | Q(product_name__icontains=keyword))
+            products_count = products.count()
+    context = {
+        'products' : products,
+        'products_count' : products_count,
+    }    
+    return render(request, 'store/store.html', context)
