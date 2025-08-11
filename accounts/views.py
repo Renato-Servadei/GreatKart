ba651d1092fa8ebf645ef07f5c3e4976dca330f5
Rober <boblawyer1@gmail.com>
2025-08-11 14:49:10 -0300

    Add password confirmation validation to registration form; implement user creation in register view and display form errors in template

--- accounts/views.py
+++ accounts/views.py
@@ -1,8 +1,23 @@
 from django.shortcuts import render
 from .forms import RegistrationForm
+from .models import Account
 
 def register(request):
-    form = RegistrationForm()
+    if request.method == 'POST':
+        form = RegistrationForm(request.POST)
+        if form.is_valid():
+            first_name = form.cleaned_data['first_name']
+            last_name = form.cleaned_data['last_name']
+            email = form.cleaned_data['email']
+            phone_number = form.cleaned_data['phone_number']
+            
+            username = email.split('@')[0]
+            user = Account.objects.create(first_name = first_name, last_name = last_name, email = email, username = username)
+            user.phone_number = phone_number
+            user.set_password(form.cleaned_data['password'])
+            user.save()
+    else:
+        form = RegistrationForm()
     context = {
         'form' : form,
     }
